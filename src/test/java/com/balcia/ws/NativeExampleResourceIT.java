2af56373d25c7c34f8191dd4d6dbad25438ebedf
Gints Polis <gints.polis@balcia.com>
2019-09-25 08:26:48 +0300

    Generated base

---
+++ src/test/java/com/balcia/ws/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.balcia.ws;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
