704a1640457b5bc24203035b037b5361c0a06263
Gints Polis <gints.polis@balcia.com>
2019-11-26 11:38:07 +0200

    Import script contains related colour.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,7 @@
-INSERT INTO known_fruits(id, name) VALUES (1, 'Cherry');
-INSERT INTO known_fruits(id, name) VALUES (2, 'Apple');
-INSERT INTO known_fruits(id, name) VALUES (3, 'Banana');
+INSERT INTO color (id, name) VALUES (1, 'Red');
+INSERT INTO color (id, name) VALUES (2, 'Green');
+INSERT INTO color (id, name) VALUES (3, 'Yellow');
+
+INSERT INTO known_fruits(id, name, color_id) VALUES (1, 'Cherry', 1);
+INSERT INTO known_fruits(id, name, color_id) VALUES (2, 'Apple', 2);
+INSERT INTO known_fruits(id, name, color_id) VALUES (3, 'Banana', 3);
